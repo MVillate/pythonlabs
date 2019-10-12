f7116eb19259cf47170664d92b8a4b6593be392e
Manuel <manuelvillate@MV.local>
2019-10-12 13:14:23 +0200

    Adding variable scopes in Functions

--- functions.py
+++ functions.py
@@ -1,2 +1,13 @@
-def square(num):
-    return num*num
+'''
+LEGB
+Local Enclosing Global Built-in
+'''
+
+def triple(num):
+    return num*num*num
+
+def arg_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+arg_info(5,6,[1,2,3], names=['manu', 'fer'])
